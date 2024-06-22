80876a5ee59e53cdf7c9fd2b913fb68bc9a6a23e
Shivani622 <shivani080202@gmail.com>
2024-06-22 15:59:55 +0530

    authentication

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const {chats} = require("./data/data");
+const {connectDB} = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler} = require('./middleware/errorMiddleware');
+
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-dotenv.config();
+
+app.use(express.json());  // to accept json data
+
+app.use('/api/user', userRoutes)
+
 
 app.get('/', (req, res)=>{
     res.send("API is running");
 })
 
-app.get('/api/chat', (req, res)=>{
-    res.send(chats);
-})
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/chat/:id',(req, res)=>{
-    // console.log(req.params.id);
-    const singleChat = chats.find(c=> c._id ==req.params.id);
-    res.send(singleChat);
-})
+// app.get('/api/chat/:id',(req, res)=>{
+//     // console.log(req.params.id);
+//     const singleChat = chats.find(c=> c._id ==req.params.id);
+//     res.send(singleChat);
+// })
 
 const PORT = process.env.PORT || 5000
-app.listen(5000, console.log(`Server Started on PORT 5000 ${PORT}`));
+app.listen(5000, console.log(`Server Started on PORT 5000 ${PORT}`.yellow.bold));
